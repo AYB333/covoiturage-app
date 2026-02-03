230e6d20f27bb3a91b77fe0c46615b3c87af918e
ayoub <aitomghar26@gmail.com>
2026-02-03 16:30:28 +0100

    mongodb connected successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();   // 👈 هادي هي المهمة
+
+connectDB();
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
